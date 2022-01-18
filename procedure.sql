3456a28be35209d89bc6686a4d62fc3123fbba06
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-18 12:21:00 +0200

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,1 +1,15 @@
+--ця процедура отримує ім'я команды та її бюджет витрачений на гравців щомісячно 
 
+
+CREATE OR REPLACE PROCEDURE count_sum_of_wage_teams(t_name char(50)) 
+LANGUAGE 'plpgsql'
+AS $$
+DECLARE sum_of_pts_reb_ast integer;
+
+BEGIN
+	SELECT player_wage INTO sum_of_pts_reb_ast
+	FROM players
+	WHERE players.player_name = t_name;
+    RAISE INFO 'Players name: %,  Sum of player_wage: %', t_name,  sum_of_pts_reb_ast;
+END;
+$$;
