8a69d2c54890409829ce0695e2c99417313f6b5a
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-18 15:08:17 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,1 +1,639 @@
 
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (0, 'FC Bayern München');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (1, 'Paris Saint-Germain');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (2, 'Manchester United');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (3, 'Tottenham Hotspur');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (4, 'Chelsea');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (5, 'Juventus');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (6, 'Atlético Madrid');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (7, 'FC Barcelona');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (8, 'Real Madrid');
+
+
+
+INSERT INTO clubs(team_id,
+                    team_name)
+VALUES (9, 'Manchester City');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (0, 'Uruguay');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (1, 'Portugal');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (2, 'Spain');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (3, 'Slovenia');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (4, 'Argentina');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (5, 'Belgium');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (6, 'Germany');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (7, 'Brazil');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (8, 'England');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (9, 'Italy');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (10, 'Croatia');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (11, 'France');
+
+
+
+INSERT INTO countries(country_id,
+                    country_name)
+VALUES (12, 'Poland');
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (158023, 
+        'https://cdn.sofifa.org/players/4/19/158023.png',
+        'Medium/ Medium' ,
+        'L. Messi',
+         31,
+        '€565K', 
+        7, 
+        4);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (20801, 
+        'https://cdn.sofifa.org/players/4/19/20801.png',
+        'High/ Low' ,
+        'Cristiano Ronaldo',
+         33,
+        '€405K', 
+        5, 
+        1);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (190871, 
+        'https://cdn.sofifa.org/players/4/19/190871.png',
+        'High/ Medium' ,
+        'Neymar Jr',
+         26,
+        '€290K', 
+        1, 
+        7);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (193080, 
+        'https://cdn.sofifa.org/players/4/19/193080.png',
+        'Medium/ Medium' ,
+        'De Gea',
+         27,
+        '€260K', 
+        2, 
+        2);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (192985, 
+        'https://cdn.sofifa.org/players/4/19/192985.png',
+        'High/ High' ,
+        'K. De Bruyne',
+         27,
+        '€355K', 
+        9, 
+        5);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (183277, 
+        'https://cdn.sofifa.org/players/4/19/183277.png',
+        'High/ Medium' ,
+        'E. Hazard',
+         27,
+        '€340K', 
+        4, 
+        5);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (177003, 
+        'https://cdn.sofifa.org/players/4/19/177003.png',
+        'High/ High' ,
+        'L. Modrić',
+         32,
+        '€420K', 
+        8, 
+        10);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (176580, 
+        'https://cdn.sofifa.org/players/4/19/176580.png',
+        'High/ Medium' ,
+        'L. Suárez',
+         31,
+        '€455K', 
+        7, 
+        0);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (155862, 
+        'https://cdn.sofifa.org/players/4/19/155862.png',
+        'High/ Medium' ,
+        'Sergio Ramos',
+         32,
+        '€380K', 
+        8, 
+        2);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (200389, 
+        'https://cdn.sofifa.org/players/4/19/200389.png',
+        'Medium/ Medium' ,
+        'J. Oblak',
+         25,
+        '€94K', 
+        6, 
+        3);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (188545, 
+        'https://cdn.sofifa.org/players/4/19/188545.png',
+        'High/ Medium' ,
+        'R. Lewandowski',
+         29,
+        '€205K', 
+        0, 
+        12);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (182521, 
+        'https://cdn.sofifa.org/players/4/19/182521.png',
+        'Medium/ Medium' ,
+        'T. Kroos',
+         28,
+        '€355K', 
+        8, 
+        6);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (182493, 
+        'https://cdn.sofifa.org/players/4/19/182493.png',
+        'Medium/ High' ,
+        'D. Godín',
+         32,
+        '€125K', 
+        6, 
+        0);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (168542, 
+        'https://cdn.sofifa.org/players/4/19/168542.png',
+        'High/ Medium' ,
+        'David Silva',
+         32,
+        '€285K', 
+        9, 
+        2);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (215914, 
+        'https://cdn.sofifa.org/players/4/19/215914.png',
+        'Medium/ High' ,
+        'N. Kanté',
+         27,
+        '€225K', 
+        4, 
+        11);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (211110, 
+        'https://cdn.sofifa.org/players/4/19/211110.png',
+        'High/ Medium' ,
+        'P. Dybala',
+         24,
+        '€205K', 
+        5, 
+        4);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (202126, 
+        'https://cdn.sofifa.org/players/4/19/202126.png',
+        'High/ High' ,
+        'H. Kane',
+         24,
+        '€205K', 
+        3, 
+        8);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (194765, 
+        'https://cdn.sofifa.org/players/4/19/194765.png',
+        'High/ High' ,
+        'A. Griezmann',
+         27,
+        '€145K', 
+        6, 
+        11);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (192448, 
+        'https://cdn.sofifa.org/players/4/19/192448.png',
+        'Medium/ Medium' ,
+        'M. ter Stegen',
+         26,
+        '€240K', 
+        7, 
+        6);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (192119, 
+        'https://cdn.sofifa.org/players/4/19/192119.png',
+        'Medium/ Medium' ,
+        'T. Courtois',
+         26,
+        '€240K', 
+        8, 
+        5);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (189511, 
+        'https://cdn.sofifa.org/players/4/19/189511.png',
+        'Medium/ Medium' ,
+        'Sergio Busquets',
+         29,
+        '€315K', 
+        7, 
+        2);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (179813, 
+        'https://cdn.sofifa.org/players/4/19/179813.png',
+        'High/ High' ,
+        'E. Cavani',
+         31,
+        '€200K', 
+        1, 
+        0);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (167495, 
+        'https://cdn.sofifa.org/players/4/19/167495.png',
+        'Medium/ Medium' ,
+        'M. Neuer',
+         32,
+        '€130K', 
+        0, 
+        6);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (153079, 
+        'https://cdn.sofifa.org/players/4/19/153079.png',
+        'High/ Medium' ,
+        'S. Agüero',
+         30,
+        '€300K', 
+        9, 
+        4);
+
+
+
+
+        INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,
+                    team_id,
+                    country_id)
+VALUES (138956, 
+        'https://cdn.sofifa.org/players/4/19/138956.png',
+        'Medium/ High' ,
+        'G. Chiellini',
+         33,
+        '€215K', 
+        5, 
+        9);
+
+
+
+
