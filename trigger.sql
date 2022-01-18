3e2f895b7c66cb812fa6e93437b2647b9ddd36c3
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-18 12:34:45 +0200

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,1 +1,18 @@
+--ця функція та триггер додають до імені гравця приставку Mr.
+CREATE OR REPLACE FUNCTION add_mr() RETURNS trigger AS
+$$
+     BEGIN
+          UPDATE players 
+          SET player_name = 'Mr. ' || player_name 
+		  WHERE players.player_id = NEW.player_id; 
+		  RETURN NULL;
+     END;
+$$ LANGUAGE 'plpgsql';
+
+
+CREATE TRIGGER player_mr_insert 
+AFTER INSERT ON players
+FOR EACH ROW EXECUTE FUNCTION add_mr();
+
+
 
