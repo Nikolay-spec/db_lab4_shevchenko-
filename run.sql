15efad1d0bb5f04389c05325f45afd7c511f263b
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-18 12:47:52 +0200

    Update run.sql

--- run.sql
+++ run.sql
@@ -1,1 +1,19 @@
+--щоб запустити це все треба запустити код з sql-файлів trigger, procedure, function
+--перевірка функції
+Select count_max_age();
+
+--перевірка процедури
+CALL count_sum_of_wage_team ('Patrick');
+
+--перевірка триггеру
+INSERT INTO Players(player_id,
+                    player_photo,
+                    player_workrate,
+                    player_name,
+                    player_age,
+                    player_wage,   
+                    country_id,
+                    team_id)
+VALUES (150230, 'https://cdn.sofifa.org/players/4/19/1580203.png','Medium/ Medium', 'L. Messii',	     310, 5650000, 0001, 0001);
+select player_name from players
 
