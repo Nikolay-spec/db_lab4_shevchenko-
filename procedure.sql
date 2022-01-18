360eeb1639a24598425fefb2a90fe06704d79f5c
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-18 13:25:12 +0200

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -4,13 +4,13 @@
 CREATE OR REPLACE PROCEDURE count_sum_of_wage_teams(t_name char(50)) 
 LANGUAGE 'plpgsql'
 AS $$
-DECLARE sum_of_pts_reb_ast integer;
+DECLARE sum_of_wage_teams;
 
 BEGIN
-	SELECT player_wage INTO sum_of_pts_reb_ast
+	SELECT player_wage INTO sum_of_wage_teams
 	FROM players,clubs
 	WHERE clubs.team_name = t_name;
-    RAISE INFO 'Players name: %,  Sum of player_wage: %', t_name,  sum_of_pts_reb_ast;
+    RAISE INFO 'Team name: %,  Sum of player_wage: %', t_name,  sum_of_wage_teams;
 END;
 $$;
-select count_sum_of_wage_teams(FC Barcelona)
+select count_sum_of_wage_teams('Juventus')
