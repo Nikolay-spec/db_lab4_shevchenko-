90878011bb2d3d79f0c152465a0c0e2793ca68bb
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-18 11:34:18 +0200

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,1 +1,17 @@
+--ця функція повертає ім'я гравця з найбільшим віком
 
+CREATE OR REPLACE FUNCTION count_max_age() RETURNS char(50) AS
+$$
+    DECLARE
+		player_name_and_id char(50);
+    BEGIN
+        
+		SELECT player_name ||' '||player_id 
+		INTO player_name_and_id
+        FROM players
+		WHERE player_age = (SELECT MAX(player_age) 
+					 FROM players);
+		
+        RETURN player_name_and_id;
+    END;
+$$ LANGUAGE 'plpgsql';
